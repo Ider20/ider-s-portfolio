ffcd532354ee021aad4145c8a64e68597088d255
Ider IshTsog <23LP0487@G304-CC0031.local>
2023-12-06 19:42:54 +0800

    making components

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,30 +8,45 @@ export default function Home() {
   return (
     <>
       {/* header nav scene*/}
-      <header>
-        <nav>
-          <div>{symbol}</div>
-          <div>
-            <ul>
-              <li>About</li>
-              <li>Work</li>
-              <li>Testimonials</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-          <div className="theme">
-            <img src="/Home/Desktop/theme_change.svg" />
-            <img src="/Home/Desktop/download.svg" />
+      <header class="w-full">
+        <nav class="flex w-9/12 m-auto justify-between mt-16 h-24 px-20 items-center rounded-lg">
+          <div class="text-3xl font-bold">{symbol}</div>
+          <div class="flex gap-32 items-center">
+            <div>
+              <ul class="flex gap-10">
+                <li class="hover:scale-110 cursor-pointer ease-in duration-100">
+                  About
+                </li>
+                <li class="hover:scale-110 cursor-pointer ease-in duration-100">
+                  Work
+                </li>
+                <li class="hover:scale-110 cursor-pointer ease-in duration-100">
+                  Testimonials
+                </li>
+                <li class="hover:scale-110 cursor-pointer ease-in duration-100">
+                  Contact
+                </li>
+              </ul>
+            </div>
+            <div class="flex gap-10">
+              <img
+                src="/Home/Desktop/theme_change.svg"
+                class="hover:scale-110 cursor-pointer ease-in duration-100"
+              />
+              <img
+                src="/Home/Desktop/download.svg"
+                class="hover:scale-105 cursor-pointer ease-in duration-100"
+              />
+            </div>
           </div>
         </nav>
-
         {/* header info scene */}
-        <div className="info">
-          <div>
+        <div class="flex w-3/4 m-auto px-20 py-6 gap-10 rounded-lg">
+          <div class="w-3/4">
             <div>
-              <h1>Hi, I'm Ider</h1>
+              <h1 class="text-4xl font-bold mb-5">Hi, I'm Ider</h1>
               <img />
-              <p>
+              <p class="mb-5 text-xs">
                 I am a full stack developer bla bla bla bla bla bla bla bla bla{" "}
                 bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
                 bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
@@ -40,23 +55,23 @@ export default function Home() {
                 bla bla bla bla bla bla bla bla{" "}
               </p>
               <ul>
-                <li>
+                <li class="flex gap-x-1 text-xs mb-2">
                   <img src="/Home/Desktop/location.svg" />
                   Ulaanbaatar, Mongolia
                 </li>
-                <li>
+                <li class="flex ml-2 gap-x-3 text-xs mb-8">
                   <img src="/Home/Desktop/dot.svg" />
                   Available for new project
                 </li>
               </ul>
             </div>
-            <div className="icons">
+            <div class="flex w-5 gap-x-3 ml-1">
               <img src="/Home/Desktop/github.svg" />
               <img src="/Home/Desktop/twitter.svg" />
               <img src="/Home/Desktop/figma.svg" />
             </div>
           </div>
-          <div>
+          <div class="w-1/4 flex items-center bg-[#fff] shadow-3xl border-solid border-2 border-black-100 p-3">
             <img src="/next.svg" className="profile-photo" />
           </div>
         </div>
